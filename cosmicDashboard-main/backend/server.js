19096ee85a7c77204ddd25e35e0d575edb4ac854
AnnuKodam <anuhyakodamir25@gmail.com>
2025-06-25 16:14:52 +0100

    URL changes for deployment

--- cosmicDashboard-main/backend/server.js
+++ cosmicDashboard-main/backend/server.js
@@ -9,7 +9,10 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://nasa-frontend-kqlr.onrender.com",
+    ],
   })
 );
 app.use(express.json());
--- cosmicDashboard-main/frontend/src/api/nasaApi.js
+++ cosmicDashboard-main/frontend/src/api/nasaApi.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 const apiClient = axios.create({
-    // IMPORTANT: Replace this with your actual backend URL when deployed
-    baseURL: 'http://localhost:5000/api', 
+  // IMPORTANT: Replace this with your actual backend URL when deployed
+  baseURL: "https://nasa-backend-dzlm.onrender.com/api",
 });
 
-export const fetchApod = () => apiClient.get('/apod');
-export const fetchMarsPhotos = (params) => apiClient.get('/mars-photos', { params });
-export const fetchNeows = () => apiClient.get('/neows');
+export const fetchApod = () => apiClient.get("/apod");
+export const fetchMarsPhotos = (params) =>
+  apiClient.get("/mars-photos", { params });
+export const fetchNeows = () => apiClient.get("/neows");
